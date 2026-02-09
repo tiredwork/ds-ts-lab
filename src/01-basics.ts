3a54a65c4864b5dfceaa917d85bf252bc31eb9bf
Dion Kennedy <20101554@mail.wit.ie>
2026-02-09 14:21:27 +0000

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -7,23 +7,32 @@ interface Friend {
 
 // (Optional) Change the names below to your friends.
 
-const friend1 = {
+const friend1: Friend = {
   name: "Paul Fleming",
   phone: "087-12345",
   age: 25,
 };
 
-const friend2 = {
+const friend2: Friend = {
   name: "Jane Costello",
   phone: "086--12345",
   age: 31,
 };
 
-const friends = [friend1, friend2];
+const friends: Friend[] = [friend1, friend2];
 console.log(friends[1]);
 
 //   -------------------
-const colleague1 = {
+interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+        email: string;
+        extension: number;
+    }
+}
+
+const colleague1: Colleague = {
   name: "Ralph Graham",
   department: "Engineering",
   contact: {
@@ -32,7 +41,7 @@ const colleague1 = {
   },
 };
 
-const colleague2 = {
+const colleague2: Colleague = {
   name: "Patti Burke",
   department: "Finance",
   contact: {
@@ -41,7 +50,7 @@ const colleague2 = {
   },
 };
 
-const colleague3 = {
+const colleague3: Colleague = {
   name: "Dean Sullivan",
   department: "HR",
   contact: {
@@ -49,7 +58,13 @@ const colleague3 = {
     extension: 125,
   },
 };
-const colleagues = {
+
+interface ColleagueHistory {
+  current: Colleague[],
+  former: Colleague[]
+}
+
+export const colleagues : ColleagueHistory = {
   current: [colleague1, colleague2, colleague3],
   former: [],
 };
